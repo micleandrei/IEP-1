972f085b86b4305c91b416347f07837bfad58bc2
Dragos Ilca <dragos.ilca13@gmail.com>
2020-10-22 16:05:23 -0400

    Add first implementation of cmake projects

---
+++ cmakeLab/math/inc/mathCalc.h
@@ -0,0 +1,7 @@
+class MathCalc {
+    public:
+        int add(int a, int b);
+        int sub(int a, int b);
+        int mul(int a, int b);
+        int div(int a, int b);
+};
---
+++ cmakeLab/math/src/mathCalc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<mathCalc.h>
+
+int MathCalc::add(int a, int b) {
+    return a + b;
+}
+
+int MathCalc::sub(int a, int b) {
+    return a - b;
+}
+
+int MathCalc::mul(int a, int b) {
+    return a * b;
+}
+
+int MathCalc::div(int a, int b) {
+    return a / b;
+}
